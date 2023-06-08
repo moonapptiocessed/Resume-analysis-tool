12230711a34e78f0e75ec41d74d003468c3ef245
moonapptiocessed <135938512+moonapptiocessed@users.noreply.github.com>
2023-06-09 00:50:30 +0530

    Update Courses.py

--- Courses.py
+++ Courses.py
@@ -8,6 +8,14 @@ ds_course = [['Machine Learning Crash Course by Google [Free]', 'https://develop
              ['Programming for Data Science with R','https://www.udacity.com/course/programming-for-data-science-nanodegree-with-R--nd118'],
              ['Introduction to Data Science','https://www.udacity.com/course/introduction-to-data-science--cd0017'],
              ['Intro to Machine Learning with TensorFlow','https://www.udacity.com/course/intro-to-machine-learning-with-tensorflow-nanodegree--nd230']]
+ds_jobs = [
+    ['https://www.linkedin.com/jobs/view/3623309449', 'https://www.linkedin.com/jobs/view/3623309449'],
+    ['https://www.linkedin.com/jobs/view/3484024788', 'https://www.linkedin.com/jobs/view/3484024788'],
+    ['https://www.linkedin.com/jobs/view/3599165408', 'https://www.linkedin.com/jobs/view/3599165408'],
+    ['https://www.linkedin.com/jobs/view/3625546497', 'https://www.linkedin.com/jobs/view/3625546497'],
+    ['https://www.linkedin.com/jobs/view/3613286926', 'https://www.linkedin.com/jobs/view/3613286926']
+]
+
 
 web_course = [['Django Crash course [Free]','https://youtu.be/e1IyzVyrLSU'],
               ['Python and Django Full Stack Web Developer Bootcamp','https://www.udemy.com/course/python-and-django-full-stack-web-developer-bootcamp'],
@@ -19,6 +27,13 @@ web_course = [['Django Crash course [Free]','https://youtu.be/e1IyzVyrLSU'],
               ['Full Stack Web Developer by Udacity','https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd0044'],
               ['Front End Web Developer by Udacity','https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011'],
               ['Become a React Developer by Udacity','https://www.udacity.com/course/react-nanodegree--nd019']]
+web_jobs = [
+    ['https://www.linkedin.com/jobs/view/3624774434', 'https://www.linkedin.com/jobs/view/3624774434'],
+    ['https://www.linkedin.com/jobs/view/3592328437', 'https://www.linkedin.com/jobs/view/3592328437'],
+    ['https://www.linkedin.com/jobs/view/3625157040', 'https://www.linkedin.com/jobs/view/3625157040'],
+    ['https://www.linkedin.com/jobs/view/3532026327', 'https://www.linkedin.com/jobs/view/3532026327'],
+    ['https://www.linkedin.com/jobs/view/3626985561', 'https://www.linkedin.com/jobs/view/3626985561']
+]
 
 android_course = [['Android Development for Beginners [Free]','https://youtu.be/fis26HvvDII'],
                   ['Android App Development Specialization','https://www.coursera.org/specializations/android-app-development'],
@@ -31,6 +46,14 @@ android_course = [['Android Development for Beginners [Free]','https://youtu.be/
                   ['Flutter & Dart - The Complete Flutter App Development Course','https://www.udemy.com/course/flutter-dart-the-complete-flutter-app-development-course/'],
                   ['Flutter App Development Course [Free]','https://youtu.be/rZLR5olMR64']]
 
+android_jobs = [
+    ['https://www.linkedin.com/jobs/view/3616255426', 'https://www.linkedin.com/jobs/view/3616255426'],
+    ['https://www.linkedin.com/jobs/view/3608394533', 'https://www.linkedin.com/jobs/view/3608394533'],
+    ['https://www.linkedin.com/jobs/view/3610569715', 'https://www.linkedin.com/jobs/view/3610569715'],
+    ['https://www.linkedin.com/jobs/view/3056137272', 'https://www.linkedin.com/jobs/view/3056137272'],
+    ['https://www.linkedin.com/jobs/view/3624218557', 'https://www.linkedin.com/jobs/view/3624218557']
+]
+
 ios_course = [['IOS App Development by LinkedIn','https://www.linkedin.com/learning/subscription/topics/ios'],
               ['iOS & Swift - The Complete iOS App Development Bootcamp','https://www.udemy.com/course/ios-13-app-development-bootcamp/'],
               ['Become an iOS Developer','https://www.udacity.com/course/ios-developer-nanodegree--nd003'],
@@ -41,6 +64,14 @@ ios_course = [['IOS App Development by LinkedIn','https://www.linkedin.com/learn
               ['Learn Swift by Codecademy','https://www.codecademy.com/learn/learn-swift'],
               ['Swift Tutorial - Full Course for Beginners [Free]','https://youtu.be/comQ1-x2a1Q'],
               ['Learn Swift Fast - [Free]','https://youtu.be/FcsY1YPBwzQ']]
+ios_jobs = [
+    ['https://www.linkedin.com/jobs/view/3597481189', 'https://www.linkedin.com/jobs/view/3597481189'],
+    ['https://www.linkedin.com/jobs/view/3597463220', 'https://www.linkedin.com/jobs/view/3597463220'],
+    ['https://www.linkedin.com/jobs/view/3624020950', 'https://www.linkedin.com/jobs/view/3624020950'],
+    ['https://www.linkedin.com/jobs/view/3580232765', 'https://www.linkedin.com/jobs/view/3580232765'],
+    ['https://www.linkedin.com/jobs/view/3619071492', 'https://www.linkedin.com/jobs/view/3619071492']
+]
+
 uiux_course = [['Google UX Design Professional Certificate','https://www.coursera.org/professional-certificates/google-ux-design'],
                ['UI / UX Design Specialization','https://www.coursera.org/specializations/ui-ux-design'],
                ['The Complete App Design Course - UX, UI and Design Thinking','https://www.udemy.com/course/the-complete-app-design-course-ux-and-ui-design/'],
@@ -52,12 +83,10 @@ uiux_course = [['Google UX Design Professional Certificate','https://www.courser
                ['Adobe XD for Beginners [Free]','https://youtu.be/WEljsc2jorI'],
                ['Adobe XD in Simple Way','https://learnux.io/course/adobe-xd']]
 
-resume_videos = ['https://youtu.be/y8YH0Qbu5h4','https://youtu.be/J-4Fv8nq1iA',
-                 'https://youtu.be/yp693O87GmM','https://youtu.be/UeMmCex9uTU',
-                 'https://youtu.be/dQ7Q8ZdnuN0','https://youtu.be/HQqqQx5BCFY',
-                 'https://youtu.be/CLUsplI4xMU','https://youtu.be/pbczsLkv7Cc']
-
-interview_videos = ['https://youtu.be/Ji46s5BHdr0','https://youtu.be/seVxXHi2YMs',
-                    'https://youtu.be/9FgfsLa_SmY','https://youtu.be/2HQmjLu-6RQ',
-                    'https://youtu.be/DQd_AlIvHUw','https://youtu.be/oVVdezJ0e7w'
-                    'https://youtu.be/JZK1MZwUyUU','https://youtu.be/CyXLhHQS3KY']
+uiux_jobs = [
+    ['https://www.linkedin.com/jobs/view/3618957507', 'https://www.linkedin.com/jobs/view/3618957507'],
+    ['https://www.linkedin.com/jobs/view/3628413345', 'https://www.linkedin.com/jobs/view/3628413345'],
+    ['https://www.linkedin.com/jobs/view/3628413345', 'https://www.linkedin.com/jobs/view/3628413345'],
+    ['https://www.linkedin.com/jobs/view/3625460038', 'https://www.linkedin.com/jobs/view/3625460038'],
+    ['https://www.linkedin.com/jobs/view/3627376821', 'https://www.linkedin.com/jobs/view/3627376821']
+]
